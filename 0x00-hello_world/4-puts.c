ee4a503594f386fe24359aed44c1f14dd63a72ec
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-08 13:31:40 -0700

    updated fie

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,19 @@
 /**
  * Main Entry point
  *
+ * Description: this is the entry of the code
+ *
  * Retrun: always 0 (Success)
  */
 int main(void)
+/**
+ * Description: the function main displays the value
+ * description: the return value set to 0
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+/**
+ * this is where the code ends
+ */
