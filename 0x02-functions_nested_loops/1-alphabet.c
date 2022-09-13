dc9e54b8b885c833a290f8e84d9656c56b7450c5
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-13 14:18:40 -0700

    created file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+	int ch;
+
+	print_alphabet("prints the alphabet, in lowercase, followed by a new line :\n");
+
+	for (ch= ‘a’; ch<= ‘z’; ch++)
+
+		 putchar(ch);
+
+	 	 putchar (‘\n’ );
+
+	return (0);
+}
