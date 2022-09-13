7b86f581d0562cb13b072f327467176376a30dce
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-13 14:24:48 -0700

    included main.h header in program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -11,8 +11,7 @@ int main(void)
 	int ch;
 
 	print_alphabet("prints the alphabet, in lowercase, followed by a new line :\n");
-
-	for (ch= ‘a’; ch<= ‘z’; ch++)
+	for (ch = ‘a’; ch <= ‘z’; ch++)
 
 		 putchar(ch);
 
