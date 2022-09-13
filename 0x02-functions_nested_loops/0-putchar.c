cc02c24bf854131b50b60833f67bb07215323d6c
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-13 04:21:14 -0700

    created file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: prints _putchar
+ * Return: always 0
+ */
+int main(void)
+{
+	char c
+	{
+		for (c = _putchar)
+	}
+	printf("\n%d"; c)
+	
+		return (0)
+}
