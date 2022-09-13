6740c7746dec419976e706dd6dda3c9c5a3d6167
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-13 14:29:23 -0700

    updated code in file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,14 +8,6 @@
 void print_alphabet(void);
 int main(void)
 {
-	int ch;
-
-	print_alphabet("prints the alphabet, in lowercase, followed by a new line :\n");
-	for (ch = ‘a’; ch <= ‘z’; ch++)
-
-		 putchar(ch);
-
-	 	 putchar (‘\n’ );
-
+	print_alphabet();
 	return (0);
 }
