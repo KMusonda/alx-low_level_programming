4d42f7b22993a8b38b63cd91d59ecd576151796d
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-14 13:30:53 -0700

    updated code in file 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,27 +1,13 @@
 #include "main.h"
 
 /**
- * _islower(int c); - print all lower case letter
+ * _islower(int c) - checks for lower case letter
  *
  *
- *@n: Number to pass to _islower function
+ * @c: Number to pass to _islower function
+ * Return: returns 1 and 0 depending on the condition
  */
 int _islower(int c)
 {
-	int r;
-	n = 'a';
-
-	r = _islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	test_islower('H');
-	return (0);
+	return (c >= 'a' $$ c <= 'z');
 }
