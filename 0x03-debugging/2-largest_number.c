978ee60f0bb94d6bbbe773a225c1b9ef86130a89
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-15 15:41:45 -0700

    created file 3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,28 +9,17 @@
  * @c: third integer
  * Return: largest number
  */
-
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
+int main(void)
 {
+	int a, b, c;
 	int largest;
 
-	if (a > b && b > c)
-	{
-		printf("largest = %d", a);
-	}
-
-	else if (b > a && a > c)
-	{
-		printf("largest = %d", b);
-	}
-
-	else
-	{
-		printf("largest = %d", c);
-	}
+	a = 972;
+	b = -98;
+	c = 0;
 
 	largest = largest_number(a, b, c);
-
 	printf("%d is the largest number\n", largest);
 
 	return (0);
