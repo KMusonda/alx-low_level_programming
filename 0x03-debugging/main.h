4a61b28795387d068dc6ea27d25a2b9b20d7ebe5
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-15 15:44:00 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,6 @@ int postitive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
 void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
