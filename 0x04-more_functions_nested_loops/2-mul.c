795c676b19549b00636f99aa29a6036ed0b61a06
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-16 13:27:24 -0700

    created file 2-mul.c that contains cod which multiples two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: inpute character to be multiplied
+ * @b: input character to be multiplied
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
