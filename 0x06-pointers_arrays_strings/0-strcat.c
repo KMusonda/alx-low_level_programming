5af142dcf3903fda89c69b8e29053a5178a3f179
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-22 05:25:02 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(*dest, *src);
+	int len 0, i;
+
+	while (dest[len])
+		len++
+
+	for (i = 0; src[i] != '0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
 	return (dest);
 }
