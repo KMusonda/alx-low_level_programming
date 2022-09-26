8c87bf6b9aa5e361bf5ea1d394398e15b9f58799
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-26 12:18:28 -0700

    created 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies a memery area
+ *
+ * @dest: memory are to be coppied to
+ * @src: mamory area to be copped from
+ * @n: number of bytes to be copied
+ * Return: to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest)
+}
