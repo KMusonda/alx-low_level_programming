da9cc768f431eca20f2a3dd0eb2363e2cbbcdb97
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-26 11:55:20 -0700

    crated file 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ *
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer return to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		x[s - 1] = b;
+		n--;
+	}
+	return (s);
+}
