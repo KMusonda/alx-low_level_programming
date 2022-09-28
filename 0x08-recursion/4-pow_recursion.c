23223565f52b12f8702b0f3d9613205d3f85aebc
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-28 11:36:39 -0700

    created file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _pow_recursion - int _pow_recursion(int x, int y)
+ *
+ * @x: value to be returned
+ * @y: power of x
+ * Return: -1 if y is lower than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
