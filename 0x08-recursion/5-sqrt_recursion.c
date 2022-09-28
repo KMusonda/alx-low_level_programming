73d6e76780cb3be6f642de17f0fc2d5da95c8490
Kanyanta <kanyantamusonda72@gmail.com>
2022-09-28 12:14:58 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,22 +11,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_sqrt_recursion(0, n));
-}
-
-/**
- * _sqrt_recursion - returns the square root of the number
- *
- * @n: test number
- * @x: square number
- * Return: the square root of n
- */
-
-int _sqrt_recursion(int n, int x)
-{
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt_recursion(n + 1, x));
+	return (_sqrt(0, n));
 }
