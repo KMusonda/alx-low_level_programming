417b8c395f8741d4bc64631093c666d942c37cdc
Kanyanta <kanyantamusonda72@gmail.com>
2022-10-03 12:01:54 -0700

    created file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <sdtio.h>
+/**
+ * main - print the name of the program
+ *
+ * @argc: number of arguments passed to fuction
+ * @argv: arument vector of pointer to string
+ * Return: 0
+ */
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this readme.md contians informatio about argv
